c79e5c27e5f20ee2388a143df9d7376a9bdf61e5
rweiland <weiland.ron@gmail.com>
2015-11-18 15:59:56 -0800

    Added the node express webserver

---
+++ webserver.js
@@ -0,0 +1,10 @@
+// Quick and dirty node.js static web server
+
+var express = require('express');
+ 
+var server = express();
+server.use(express.static(__dirname + '/public'));
+var port = 8181;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
