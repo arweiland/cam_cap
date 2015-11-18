59e1ae7a8ccba305aab8ca887120e02278ecc855
rweiland <weiland.ron@gmail.com>
2015-11-18 15:57:18 -0800

    First commit of camera capture / email sending

---
+++ cam_capture.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+
+# First attempt to keep a circular buffer and write .jpg file while recording.
+# This version records 10 seconds in buffer.  It writes the jpeg when it receives
+# a SIGINT2, waits trigger_delay period, then save the h.264 file.
+#
+# Ron Weiland, 09/21/15
+
+
+import io
+import picamera
+import signal
+import subprocess
+import datetime
+from send_gmail import send_gmail
+
+trigger_delay = 3            # How long after trigger before writing video
+video_length = 10            # How long to record video
+
+write_now = False
+exit_now = False
+
+image_file = 'public/foo.jpg'
+small_image_file = 'public/foo_small.jpg'
+video_file = 'public/foo.h264'
+mp4_file = 'public/foo.mp4'
+
+def write_image():
+    # use_video_port = True to keep the camera from having to change modes
+    camera.capture(image_file, use_video_port=True)
+    print "Wrote Image"
+
+def downsize_image():
+    print "Downsizing image"
+    cmd = ["convert", image_file, "-resize", "220", small_image_file]
+    subprocess.call( cmd )
+    print "Convert returned"
+
+def wrap_video():
+# MP4Box -fps x -add <video_file> -new <output_file.mp4>
+    print "Containerizing video file"
+    cmd = ["MP4Box", "-fps", "30", "-add", video_file, "-new", mp4_file]
+    ret = subprocess.call( cmd )
+    print "MP4Box returned"
+
+def write_video( stream ):
+    print "Started writing file"
+    with stream.lock:               # (in case we haven't stopped recording)
+        # Find the first header frame in video
+        for frame in stream.frames:
+            if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                stream.seek(frame.position)
+                break
+
+        with io.open( video_file, 'wb' ) as output:
+            output.write(stream.read())
+
+    # Wipe the circular stream once we are done
+    print "Ended writing file"
+    stream.seek(0)
+    stream.truncate()
+
+
+# This handler is entered when the program receives a SIGUSR2 signal
+def signal_handler( signal, frame ):
+    global write_now
+    print "Got signal!"
+    write_now = True
+
+# This handler is entered when the program receives a SIGINT signal (^c)
+def exit_handler( signal, frame ):
+    global exit_now
+    print "Got exit signal!"
+    exit_now = True
+
+# Register the signal handler
+signal.signal(signal.SIGUSR2, signal_handler)
+
+# Register the exit handler
+signal.signal(signal.SIGINT | signal.SIGTERM, exit_handler)
+
+# Start the camera
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.bitrate = 17000000
+#    camera.bitrate =  5000000
+#    camera.framerate = 10
+
+    #Calculate the seconds based on resoution (at 30 fps)
+    image_sec = 10                 # time in seconds
+    secs = int((camera.resolution[0] * camera.resolution[1] * image_sec) / (1920 * 1080))
+
+    # Note: PiCameraCircularIO calcuates buffer as: size = bitrate * seconds // 8
+
+    print "Seconds = ", secs
+#    bsize = (camera.bitrate * 10) / 8
+    print "Size = ", camera.bitrate * secs // 8
+    print "Resolution: ", camera.resolution, "Frame rate: ", camera.framerate
+#    stream = picamera.PiCameraCircularIO(camera, seconds=secs )
+
+    bsize = 850000 * video_length
+    stream = picamera.PiCameraCircularIO(camera, size = bsize )
+    camera.start_recording(stream, format='h264')
+
+    try:
+        print "%s Camera capture started" % (datetime.datetime.now())
+        while 1:
+            camera.wait_recording(.1)
+            if write_now:
+                write_image();                             # Write the jpg image
+                camera.wait_recording( trigger_delay )     # Wait delay period
+                camera.stop_recording()                    # Stop the recording
+                write_video( stream );                     # Save the video capture
+                wrap_video();                              # wrap it in an MP4 container
+                downsize_image();                          # downsize the jpg image
+                send_gmail( "email.html", "Alert from CLX", "public/foo_small.jpg" );  # Send out the mail
+                print "Restarting video"
+                camera.start_recording(stream, format='h264')                
+                write_now = False
+            if exit_now:
+                break
+
+    finally:
+        print "Stopped camera"
+        camera.stop_recording()
+
---
+++ send_gmail.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+
+# This is a CLX module for sending an email alert when the camera module is triggered.
+# It sends an email with a jpg attachment along with an HTML image and link back to the captured video
+#
+#  Ron Weiland, 11/12/15
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import smtplib
+
+
+import os
+import sys
+import configs
+
+email_str = ''
+our_ip = ''
+config = {}
+
+# Read configuration file upon module load
+
+def read_config():
+   global config
+   config = configs.read_config( "email.json" )
+   if ( config ):
+      print "Email Config read"
+
+def get_our_ip():
+   global our_ip
+   import socket
+   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   s.connect(('8.8.8.8', 80))           # Connect to Google's DNS
+   our_ip = s.getsockname()[0]
+   s.close()
+   print "Our IP: ", our_ip
+
+def read_email_template( fname ):
+   global email_str
+   from string import Template
+   with open( fname, "r" ) as email:
+      text = email.read()
+      s = Template( text )
+      # Substitute our IP address and the web server port into email template
+      print "IP: %s, port: %s" % (our_ip, config["link_port"])
+      email_str = s.substitute( ip_addr=our_ip, 
+                                port=config["link_port"], 
+                                location=config["location"] )
+
+def mail(to, subject, text, attach):
+   msg = MIMEMultipart('related')           # Note: 'related'
+   msg['Subject'] = subject
+   msg['From'] = config["gmail_user"]
+   msg['To'] = to
+   msg.preamble = 'This is a multi-part message in MIME format.'
+
+
+   # Encapsulate the plain and HTML versions of the message body in an
+
+   # 'alternative' part, so message agents can decide which they want to display.
+
+   msgAlternative = MIMEMultipart('alternative')
+   msg.attach(msgAlternative)
+
+   msgText = MIMEText('This is an alternative plain text message.')
+   msgAlternative.attach(msgText)
+
+   # Attach the HTML from the file
+
+   msgText = MIMEText( text, 'html' )
+   msgAlternative.attach(msgText)
+
+   # msg.attach(MIMEText(text, 'html'))          # attach message as HTML
+
+   # Read in the attachment
+   fp = open(attach, 'rb')
+   msgImage = MIMEImage(fp.read())
+   fp.close()
+
+   # Define the image's ID as referenced in the HTML
+
+   msgImage.add_header('Content-ID', '<image1>')
+   msg.attach(msgImage)
+
+
+   # Encode the attachment
+#   if attach:
+#      part = MIMEBase('application', 'octet-stream')
+#      part.set_payload(open(attach, 'rb').read())
+#      Encoders.encode_base64(part)
+#      part.add_header('Content-Disposition',
+#                      'attachment; filename="%s"' % os.path.basename(attach))
+#      msg.attach(part)
+
+   try:
+      mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+      mailServer.ehlo()
+      mailServer.starttls()
+      mailServer.ehlo()
+   except SMTPConnectError as e:
+      print "SMTP failed: ", e
+
+   mailServer.login(config["gmail_user"], config["gmail_pwd"])
+   mailServer.sendmail(config["gmail_user"], to.split(','), msg.as_string())
+
+   # Should be mailServer.quit(), but that crashes...
+   mailServer.close()
+
+def send_gmail( template, attachment=None ):
+   read_email_template( template )
+   mail(config["addresses"], config["subject"], email_str, attachment )
+
+
+#mail("some.person@some.address.com",
+#   "Hello from python!",
+#   "This is a email sent with python",
+#   "my_picture.jpg")
+
+read_config()              # Read the configuration file
+get_our_ip()               # Get our IP address (for email links)
+
+if __name__ == "__main__":
+   if len( sys.argv ) < 3:
+      print "Please give name of email file to send and image to attach"
+      sys.exit(-1)
+
+   send_gmail( sys.argv[1], sys.argv[2] )
+
+else:
+   print "Email module initialized"
